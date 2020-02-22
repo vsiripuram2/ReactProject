295471cc65faf0e36a188709b9f7fdb6b8a1a604
venkatsrpm <venkat.uideve@gmail.com>
2020-02-22 20:23:18 +0530

    changesadd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+  render() {
+    return(
+          <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +22,13 @@ function App() {
           Learn Reactuuuuuuuuuuuuuu
         </a>
       </header>
+      <Person />
     </div>
-  );
+    );
+
+  }
 }
+
+
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () =>{
+    return <h1>I am going to complete it</h1>
+};
+
+export default person;
